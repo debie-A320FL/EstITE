0adcf17be40c1799fbb8fc466cc8e6643adaed9d
debie-A320FL <wistan.pomel@gmail.com>
2025-04-22 12:51:43 +0000

    Setup 1 with logit regression

--- Simulations_Stage/Setup 1/Code/Python/ACTGdata_GPs.py
+++ Simulations_Stage/Setup 1/Code/Python/ACTGdata_GPs.py
@@ -5,40 +5,36 @@ import time
 import os
 from pathlib import Path
 
+import statsmodels.api as sm
+import patsy
+
 from sklearn import preprocessing
 from scipy import stats as sts
 from models.causal_models import CMGP
 
-
 # Evaluation Functions
 def bias(T_true, T_est):
-    return np.mean(100*T_true.reshape((-1, 1)) - 100*T_est.reshape((-1, 1)))
-
+    return np.mean(100 * T_true.reshape((-1, 1)) - 100 * T_est.reshape((-1, 1)))
 
 def PEHE(T_true, T_est):
-    return np.sqrt(np.mean((100*T_true.reshape((-1, 1)) - 100*T_est.reshape((-1, 1))) ** 2))
-
+    return np.sqrt(np.mean((100 * T_true.reshape((-1, 1)) - 100 * T_est.reshape((-1, 1))) ** 2))
 
 def MC_se(x, B):
     return sts.t.ppf(0.975, B - 1) * np.std(np.array(x)) / np.sqrt(B)
 
-
 def r_loss(y, mu, z, pi, tau):
-    return np.mean( ( (y - mu) - (z - pi)*tau )**2 )
-
+    return np.mean(((y - mu) - (z - pi) * tau) ** 2)
 
 # Options
-B = 7  # Num of simulations
+B = 80  # Num of simulations
 
 # Load AIDS data
-#basedir = str(Path(os.getcwd()).parents[2])
 basedir = "/home/onyxia/work/EstITE/Simulations_Stage/Setup 1/Data"
 data = pd.read_csv(basedir + "/simulated_1M_data.csv")
 
 # The dataset is too large
 # Randomly sample 1000 rows from the DataFrame
-data = data.sample(n=10000, random_state=1)  # You can set a random_state for reproducibility
-#AIDS = "/home/onyxia/work/EstITE/Simulations/ACTG/Data/ACTGData.csv"
+data = data.sample(n=1000, random_state=1)  # You can set a random_state for reproducibility
 
 # To save result
 # Define the full path
@@ -51,14 +47,14 @@ myZ = np.array(data["treatment"])
 # Define response
 myY = np.array(data["Y"])
 # Convert X to array
-myX = np.array(data.drop(columns=["treatment"]))
+myX = np.array(data.drop(columns=["treatment", "Y"]))
 
 # Scale numeric
 # to_scale = ["age", "wtkg", "preanti"]
 # AIDS[to_scale] = preprocessing.scale(AIDS[to_scale])
 
 # Pred and obs
-N, P = data.drop(columns=["treatment"]).shape
+N, P = data.drop(columns=["treatment", "Y"]).shape
 
 # Importer les hyperparamètres
 hyperparams = pd.read_csv(basedir + "/hyperparams.csv")
@@ -82,31 +78,27 @@ tau = (hyperparams['delta_0'].values[0] * np.ones(myX.shape[0]) +
 
 ITE = mu_0 + tau * myZ
 
-# Ajouter une colonne pi pour la probabilité théorique
-# data['pi'] = 1 / (1 + np.exp(-(mu_0 + tau * myZ)))
-
 # Calculer ITE_proba
 ITE_proba = 1 / (1 + np.exp(-(mu_0 + tau))) - 1 / (1 + np.exp(-mu_0))
 
-
 # Results storage
-# esti = ['CATT', 'CATC']; subs = ['Train', 'Test']; loss = ['Bias', 'PEHE', 'RLOSS']
-esti = ['CATT', 'CATC']; subs = ['Train', 'Test']; loss = ['Bias', 'PEHE'] # RLOSS not stored atm
+esti = ['CATT', 'CATC']
+subs = ['Train', 'Test']
+loss = ['Bias', 'PEHE']  # RLOSS not stored atm
 
 Results = {}
 for i in range(2):
     for k in range(2):
         for j in loss:
-            dest = {'%s_%s_%s' % (esti[i], subs[k], j): np.zeros((B, 2))}
+            dest = {'%s_%s_%s' % (esti[i], subs[k], j): np.zeros((B, 3))}  # 3 models: CMGP, NSGP, Logistic
             Results.update(dest)
-
 
 ##### Simulation Study
 start = time.time()
 
 for i in range(B):
 
-    print("\n*** Iteration", i+1)
+    print("\n*** Iteration", i + 1)
 
     # Set seed
     np.random.seed(100 + i)
@@ -126,15 +118,14 @@ for i in range(B):
     ITE_train = np.array(ITE_proba[split])
     ITE_test = np.array(ITE_proba[~split])
 
-    CATT_Train = ITE_train[z_train == 1]; CATC_Train = ITE_train[z_train == 0]
-    CATT_Test = ITE_test[z_test == 1]; CATC_Test = ITE_test[z_test == 0]
+    CATT_Train = ITE_train[z_train == 1]
+    CATC_Train = ITE_train[z_train == 0]
+    CATT_Test = ITE_test[z_test == 1]
+    CATC_Test = ITE_test[z_test == 0]
 
     # 1) CMGP
-    # print("CMGP")
     myCMGP = CMGP(dim=P, mode="CMGP", mod='Multitask', kern='RBF')
-    #print("before fit")
     myCMGP.fit(X=x_train, Y=y_train, W=z_train)
-    #print("after fit")
 
     train_CMGP_est = myCMGP.predict(x_train)[0]
     test_CMGP_est = myCMGP.predict(x_test)[0]
@@ -152,8 +143,7 @@ for i in range(B):
 
     Results['CATC_Test_Bias'][i, 0] = bias(CATC_Test, test_CMGP_est.reshape(-1)[z_test == 0])
     Results['CATC_Test_PEHE'][i, 0] = PEHE(CATC_Test, test_CMGP_est.reshape(-1)[z_test == 0])
-    
-    
+
     # 2) NSGP
     myNSGP = CMGP(dim=P, mode="NSGP", mod='Multitask', kern='Matern')
     myNSGP.fit(X=x_train, Y=y_train, W=z_train)
@@ -175,19 +165,81 @@ for i in range(B):
     Results['CATC_Test_Bias'][i, 1] = bias(CATC_Test, test_NSGP_est.reshape(-1)[z_test == 0])
     Results['CATC_Test_PEHE'][i, 1] = PEHE(CATC_Test, test_NSGP_est.reshape(-1)[z_test == 0])
 
+    # 3) Logistic Regression with Interaction Terms using patsy
+    # Create DataFrames for train and test sets
+    df_train = pd.DataFrame(x_train, columns=column_names)
+    df_train['treatment'] = z_train
+    df_train['y'] = y_train
+
+    df_test = pd.DataFrame(x_test, columns=column_names)
+    df_test['treatment'] = z_test
+    df_test['y'] = y_test
+
+    # Define the formula with interaction terms
+    formula = 'y ~ age + weight + comorbidities + gender + treatment + treatment:age + treatment:weight + treatment:comorbidities + treatment:gender'
+
+    # Create design matrices
+    y_train_patsy, X_train_patsy = patsy.dmatrices(formula, df_train, return_type='dataframe')
+    y_test_patsy, X_test_patsy = patsy.dmatrices(formula, df_test, return_type='dataframe')
+
+    # Fit logistic regression model
+    logit_model = sm.Logit(y_train_patsy, X_train_patsy)
+    logit_result = logit_model.fit()
+
+    # Create copies of the DataFrames with treatment set to 1 and 0
+    df_train_t1 = df_train.copy()
+    df_train_t0 = df_train.copy()
+    df_test_t1 = df_test.copy()
+    df_test_t0 = df_test.copy()
+
+    df_train_t1['treatment'] = 1
+    df_train_t0['treatment'] = 0
+    df_test_t1['treatment'] = 1
+    df_test_t0['treatment'] = 0
+
+    # Recreate design matrices with updated treatment values
+    _, X_train_patsy_t1 = patsy.dmatrices(formula, df_train_t1, return_type='dataframe')
+    _, X_train_patsy_t0 = patsy.dmatrices(formula, df_train_t0, return_type='dataframe')
+    _, X_test_patsy_t1 = patsy.dmatrices(formula, df_test_t1, return_type='dataframe')
+    _, X_test_patsy_t0 = patsy.dmatrices(formula, df_test_t0, return_type='dataframe')
+
+    # Predict probabilities for treatment and control
+    train_logit_est_t1 = logit_result.predict(X_train_patsy_t1).values
+    train_logit_est_t0 = logit_result.predict(X_train_patsy_t0).values
+    test_logit_est_t1 = logit_result.predict(X_test_patsy_t1).values
+    test_logit_est_t0 = logit_result.predict(X_test_patsy_t0).values
+
+    # Compute treatment effect
+    train_logit_est = train_logit_est_t1 - train_logit_est_t0
+    test_logit_est = test_logit_est_t1 - test_logit_est_t0
+
+    # CATT
+    Results['CATT_Train_Bias'][i, 2] = bias(CATT_Train, train_logit_est[z_train == 1])
+    Results['CATT_Train_PEHE'][i, 2] = PEHE(CATT_Train, train_logit_est[z_train == 1])
+
+    Results['CATT_Test_Bias'][i, 2] = bias(CATT_Test, test_logit_est[z_test == 1])
+    Results['CATT_Test_PEHE'][i, 2] = PEHE(CATT_Test, test_logit_est[z_test == 1])
+
+    # CATC
+    Results['CATC_Train_Bias'][i, 2] = bias(CATC_Train, train_logit_est[z_train == 0])
+    Results['CATC_Train_PEHE'][i, 2] = PEHE(CATC_Train, train_logit_est[z_train == 0])
+
+    Results['CATC_Test_Bias'][i, 2] = bias(CATC_Test, test_logit_est[z_test == 0])
+    Results['CATC_Test_PEHE'][i, 2] = PEHE(CATC_Test, test_logit_est[z_test == 0])
 
 elapsed = time.time() - start
-print("\n\nElapsed time (in h) is", round(elapsed/3600, 2)) # 2h for B=100
+print("\n\nElapsed time (in h) is", round(elapsed / 3600, 2))  # 2h for B=100
 
-models = ['CMGP', 'NSGP']
+models = ['CMGP', 'NSGP', 'Logistic']
 summary = {}
 
 for name in Results.keys():
     PD_results = pd.DataFrame(Results[name], columns=models)
-    PD_results.to_csv(basedir + "/../Results/GP_%s_%s.csv" % (B, name), index=False, header=True)
+    PD_results.to_csv(os.path.join(results_dir, "GP_%s_%s.csv" % (B, name)), index=False, header=True)
 
     aux = {name: {'CMGP': np.c_[np.mean(PD_results['CMGP']), MC_se(PD_results['CMGP'], B)],
-                  'NSGP': np.c_[np.mean(PD_results['NSGP']), MC_se(PD_results['NSGP'], B)]}}
+                  'NSGP': np.c_[np.mean(PD_results['NSGP']), MC_se(PD_results['NSGP'], B)],
+                  'Logistic': np.c_[np.mean(PD_results['Logistic']), MC_se(PD_results['Logistic'], B)]}}
     summary.update(aux)
 
 print(pd.DataFrame(summary).T)
